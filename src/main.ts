f249594a3ed9d7e771235f4830af0e56e334a4c1
litianhua <18702971504@163.com>
2022-01-27 22:15:49 +0800

    增加控制器

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const listenPort = 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(listenPort, () => {
+    console.log('在3000端口启动');
+  });
 }
 bootstrap();
