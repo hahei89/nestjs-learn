65b701f0e9da7192fa5e20501e6792981bb57066
litianhua <18702971504@163.com>
2022-01-28 11:54:34 +0800

    传参

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Request } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   @Get()
   index() {
     return '用户中心';
+  }
+  // 通过@Query()装饰器获取传参
+  @Get('add')
+  addData(@Query() query) {
+    console.log('query ==>', query);
+
+    return '增加数据';
+  }
+
+  @Get('edit')
+  editData(@Request() req) {
+    console.log(req.query);
+    return '编辑数据';
+  }
+
+  @Post('create')
+  createData(@Body() body) {
+    console.log(body);
+    return '创建数据';
   }
 }
